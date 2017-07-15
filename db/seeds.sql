e6f5b85617114998c23dc064c7675a4175fd028b
Marina James <MarinaJames@Marinas-MacBook-Air.local>
2017-07-14 20:06:02 -0400

    add in original burger code

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('The Final Kraut Down Burger');
+INSERT INTO burgers (burger_name) VALUES ('Itsy Bitsy Teeny Weenie Yellow Polka Dot Zucchini Burger');
+INSERT INTO burgers (burger_name) VALUES ('Let’s Give ‘Em Something Shiitake ‘Bout Burger');
